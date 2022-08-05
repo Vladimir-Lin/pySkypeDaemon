b7e6729599e58515b54c9148c71fe07bc2111bf6
Vladimir Lin <lin.vladimir@gmail.com>
2022-08-05 21:02:22 +0800

    Prepare Basic Layouts

---
+++ BuildPackage.bat
@@ -0,0 +1,13 @@
+echo "Upgrade ERP Maintainer"
+D:
+echo "==================================================="
+echo "Upgrade Python ERP Maintainer for Actions Language"
+cd D:\Programs\ERP
+git pull https://actionslanguage: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@github.com/actionslanguage/PyERP
+echo "==================================================="
+echo "Upgrade Python for Actions Language"
+cd D:\Programs\ERP\Libs\Actions
+git pull https://actionslanguage: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@github.com/actionslanguage/PyActions
+echo "==================================================="
+cd \Programs\ERP
+echo "Completed Upgrade ERP Maintainer"
--- pySkypeDaemon.pro
+++ pySkypeDaemon.pro
@@ -3,9 +3,9 @@ SOURCES += $${PWD}/*.txt
 SOURCES += $${PWD}/*.json
 SOURCES += $${PWD}/*.py
 SOURCES += $${PWD}/*.pyw
-SOURCES += $${PWD}/*.bat
 SOURCES += $${PWD}/*.MD
 SOURCES += $${PWD}/*.rst
+SOURCES += $${PWD}/*.in
 
 SOURCES += $${PWD}/pySkypeDaemon/*.py
 SOURCES += $${PWD}/pySkypeDaemon/*.pyw
